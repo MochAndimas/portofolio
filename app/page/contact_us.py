2a67a5d7f8bd32aa1e4914c271d020aaa5f2c5af
MochAndimas <mochamadandimas@gmail.com>
2024-12-20 16:20:12 +0700

    update 20 des 2024

--- app/page/contact_us.py
+++ app/page/contact_us.py
@@ -9,11 +9,18 @@ def contact_us_page():
     st.title("Contact Me")
 
     with st.form("email_form"):
+        # Input fields
         email = st.text_input("Email")
         subject = st.text_input("Subject")
         message = st.text_area("Message")
-        submitted = st.form_submit_button("Send Email")
+
+        # Check if all fields are filled
+        all_fields_filled = bool(email.strip()) and bool(subject.strip()) and bool(message.strip())
+
+        # Submit button, enabled only if all fields are filled
+        submitted = st.form_submit_button("Send Email", disabled=not all_fields_filled)
 
         if submitted:
+            # Call the email sending function
             status = send_email(email, subject, message)
             st.success(status)
